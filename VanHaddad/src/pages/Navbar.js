dbdd2e5f41945ce4d8b13afdfce6f7d2138d0572
Van Haddad <haddadvan@hotmail.com>
2020-07-10 14:42:06 -0400

    Fix nav

--- VanHaddad/src/pages/Navbar.js
+++ VanHaddad/src/pages/Navbar.js
@@ -3,12 +3,12 @@ import React from "react"
 export default function Navbar() {
   return (
     <nav className="navbar is-dark is-spaced">
-      <div className="navbar-menu">
-        <div className="navbar-start">
-          <div className="navbar-item content">
-            <h1 className="mb-0 has-text-white">Van Haddad</h1>
-          </div>
+      <div className="navbar-brand">
+        <div className="navbar-item content">
+          <h1 className="mb-0 has-text-white">Van Haddad</h1>
         </div>
+      </div>
+      <div className="navbar-menu">
         <div className="navbar-end">
           <div className="navbar-item content">
             <h3 className="mb-0 has-text-white">Senior Software Developer</h3>
--- VanHaddad/src/pages/Tags.js
+++ VanHaddad/src/pages/Tags.js
@@ -2,28 +2,28 @@ import React from "react";
 
 export default function Tags() {
   return (
-    <div class="tags are-large">
-      <span class="tag is-info">C++</span>
-      <span class="tag is-info">C#</span>
-      <span class="tag is-info">Java</span>
-      <span class="tag is-info">ASP.NET MVC</span>
-      <span class="tag is-info">ASP.NET Webforms</span>
-      <span class="tag is-info">Solr</span>
-      <span class="tag is-info">SQL</span>
-      <span class="tag is-info">Javascript</span>
-      <span class="tag is-info">.NET Core</span>
-      <span class="tag is-info">Apex</span>
-      <span class="tag is-info">KnockoutJS</span>
-      <span class="tag is-info">Azure DevOps</span>
-      <span class="tag is-info">Git/SVN</span>
-      <span class="tag is-info">Sitecore</span>
-      <span class="tag is-info">Kentico</span>
-      <span class="tag is-info">Salesforce</span>
-      <span class="tag is-info">VisualForce</span>
-      <span class="tag is-info">ReactJS</span>
-      <span class="tag is-info">Bootstrap</span>
-      <span class="tag is-info">Bulma</span>
-      <span class="tag is-info">GatsbyJS</span>
+    <div className="tags are-large">
+      <span className="tag is-info">C#</span>
+      <span className="tag is-info">C++</span>
+      <span className="tag is-info">Java</span>
+      <span className="tag is-info">ASP.NET MVC</span>
+      <span className="tag is-info">ASP.NET Webforms</span>
+      <span className="tag is-info">Solr</span>
+      <span className="tag is-info">SQL</span>
+      <span className="tag is-info">Javascript</span>
+      <span className="tag is-info">.NET Core</span>
+      <span className="tag is-info">Apex</span>
+      <span className="tag is-info">KnockoutJS</span>
+      <span className="tag is-info">Azure DevOps</span>
+      <span className="tag is-info">Git/SVN</span>
+      <span className="tag is-info">Sitecore</span>
+      <span className="tag is-info">Kentico</span>
+      <span className="tag is-info">Salesforce</span>
+      <span className="tag is-info">VisualForce</span>
+      <span className="tag is-info">ReactJS</span>
+      <span className="tag is-info">Bootstrap</span>
+      <span className="tag is-info">Bulma</span>
+      <span className="tag is-info">GatsbyJS</span>
     </div>
   )
 }
